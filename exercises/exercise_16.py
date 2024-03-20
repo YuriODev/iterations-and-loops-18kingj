2d23681349f6c9bf992c8b026f66d115772feaaf
18kingj <161600931+18kingj@users.noreply.github.com>
2024-03-20 08:01:49 +0000

    16 and 17

--- exercises/exercise_16.py
+++ exercises/exercise_16.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 16
+num = int(input())
+count = num
+if num > 6:
+    count -= 1
+string = ''
+for i in range(1, num+1):
+    count -= 1
+    string = ' ' * count + '#' * i
+    
+    print(string)
